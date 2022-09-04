d14ad55ce4d834eff1ce2e4e0502cbe82ca478b6
Bruno Morgades <brunomorgades@hotmail.com>
2022-09-04 15:49:17 -0300

    id added for game cards element, toggle function being improved

--- Part2.js
+++ Part2.js
@@ -1,21 +1,23 @@
+const toggle= (pointer)=>{
+    const parent = document.querySelector(pointer);
+    const elements=parent.querySelectorAll(':scope > div');
+    const array=[...elements];
 
-const elements=document.querySelectorAll(".info__container");
-const array=[...elements];
+    const next = (info)=>{
+        const last=array.pop();
+        array.unshift(last);
+        document.querySelector(info).innerHTML="";
+        array.forEach(item=>document.querySelector(info).appendChild(item))
+    }
+    const previous = (info)=>{
+        const last=array.shift();
+        array.push(last);
+        document.querySelector(info).innerHTML="";
+        array.forEach(item=>document.querySelector(info).appendChild(item))
+    }
 
-
-console.log("elementos",elements,elements.length,elements[0]);
-const next = ()=>{
-    const last=array.pop();
-    array.unshift(last);
-    document.querySelector(".info").innerHTML="";
-    array.forEach(item=>document.querySelector(".info").appendChild(item))
+    document.querySelector(".header__button--login").addEventListener("click", function() {next(pointer);});
+    document.querySelector(".header__button--join").addEventListener("click", function() {previous(pointer);});
 }
-const previous = ()=>{
-    const last=array.shift();
-    array.push(last);
-    document.querySelector(".info").innerHTML="";
-    array.forEach(item=>document.querySelector(".info").appendChild(item))
-}
 
-document.querySelector(".header__button--login").addEventListener("click", next);
-document.querySelector(".header__button--join").addEventListener("click", previous);
+toggle(".info");
