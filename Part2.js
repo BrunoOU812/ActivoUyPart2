26473bb4a52598d76471bf890b42dba29670ad99
Bruno Morgades <brunomorgades@hotmail.com>
2022-09-04 19:30:31 -0300

    toggle and btnArray functions completed

--- Part2.js
+++ Part2.js
@@ -1,7 +1,20 @@
-const toggle= (pointer,previousBtn,nextBtn)=>{
+const btnArray=(style)=>{
+    const regEx=/[a-z][a-z]*/gi;
+    style=style.match(regEx).join();
+    const prevBtn= document.createElement("button");
+    const nextBtn= document.createElement("button");
+    prevBtn.classList.add(`${style}__toggle`,`${style}__toggle--previous`);
+    nextBtn.classList.add(`${style}__toggle`,`${style}__toggle--next`);
+    return [prevBtn,nextBtn];
+}
+
+const toggle= (pointer)=>{
     const parent = document.querySelector(pointer);
     const elements=parent.querySelectorAll(':scope > div');
-    const btn=parent.querySelectorAll(':scope > button');
+    const btn=btnArray(pointer);
+    console.log(btn);
+    parent.prepend(btn[0]);
+    parent.appendChild(btn[1])
     const array=[...elements];
     const next = ()=>{
         const last=array.pop();
@@ -17,9 +30,8 @@ const toggle= (pointer,previousBtn,nextBtn)=>{
         array.forEach(item=>parent.appendChild(item))
         parent.appendChild(btn[1])
     }
-
-    document.querySelector(nextBtn).addEventListener("click",next);
-    document.querySelector(previousBtn).addEventListener("click",previous);
+    document.querySelector(`${pointer}__toggle--next`).addEventListener("click",next);
+    document.querySelector(`${pointer}__toggle--previous`).addEventListener("click",previous);
 }
 
-toggle(".info",".info__toggle--previous",".info__toggle--next");
+toggle(".info");
