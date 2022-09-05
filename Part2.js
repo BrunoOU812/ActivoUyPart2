71681b6f6c35267e411689a7462beb8573bf8cf6
Bruno Morgades <brunomorgades@hotmail.com>
2022-09-04 21:11:43 -0300

    toggle and btnArray working 100%

--- Part2.js
+++ Part2.js
@@ -1,6 +1,9 @@
-const btnArray=(style)=>{
+const extractWord=(word)=>{
     const regEx=/[a-z][a-z]*/gi;
-    style=style.match(regEx).join();
+    return word.match(regEx).join();
+}
+const btnArray=(style)=>{
+    style=extractWord(style);
     const prevBtn= document.createElement("button");
     const nextBtn= document.createElement("button");
     prevBtn.classList.add(`${style}__toggle`,`${style}__toggle--previous`);
@@ -12,7 +15,6 @@ const toggle= (pointer)=>{
     const parent = document.querySelector(pointer);
     const elements=parent.querySelectorAll(':scope > div');
     const btn=btnArray(pointer);
-    console.log(btn);
     parent.prepend(btn[0]);
     parent.appendChild(btn[1])
     const array=[...elements];
@@ -21,17 +23,20 @@ const toggle= (pointer)=>{
         array.unshift(last);
         elements.forEach(item=>{item.remove();})
         array.forEach(item=>parent.appendChild(item));
-        parent.appendChild(btn[1])
+        parent.appendChild(btn[1]);
     }
     const previous = ()=>{
         const last=array.shift();
         array.push(last);
         elements.forEach(item=>{item.remove();})
         array.forEach(item=>parent.appendChild(item))
-        parent.appendChild(btn[1])
+        parent.appendChild(btn[1]);
     }
-    document.querySelector(`${pointer}__toggle--next`).addEventListener("click",next);
-    document.querySelector(`${pointer}__toggle--previous`).addEventListener("click",previous);
+    const nameClass=extractWord(pointer);
+    document.querySelector(`.${nameClass}__toggle--next`).addEventListener("click",next);
+    document.querySelector(`.${nameClass}__toggle--previous`).addEventListener("click",previous);
 }
 
-toggle(".info");
+const toggleClasses= [".info","#purple", "#yellow", "#orange", "#red", "#green", "#blue","#testimonial"];
+
+toggleClasses.forEach(item=>toggle(item));
